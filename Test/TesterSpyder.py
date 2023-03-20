9a3c2d420d5f14e5212841036432f6d942ef0b2f
ericaanderson <gy22ea@leeds.ac.uk>
2023-03-20 19:58:58 -0400

    Create TesterSpyder.py

---
+++ Test/TesterSpyder.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+
+# Are there any changes?
+print("max + min")
+print(12)
+
+t = True
+f = False
+print("t == t", t == t)             # Prints True
+print("f == f", f == f)             # Prints True
+print("t == 1", t == 1)             # Prints True
+print("f == 0", f == 0)             # Prints True
+print("t != f", t != f)             # Prints True
+print("not t", not t)               # Prints False
+print("not f", not f)               # Prints True
+print("t and f", t and f)           # Prints False
+print("t and not f", t and not f)   # Prints True
+print("t or f", t or f)             # Prints True
+print("not (t or f)", not (t or f)) # Prints False
