4f63d537c2c40edcf2676cabcfbbbfd1be714e75
ericaanderson <gy22ea@leeds.ac.uk>
2023-04-03 21:25:02 -0400

    Create Document about timings.py

---
+++ src/abm3/Document about timings.py
@@ -0,0 +1,8 @@
+Brief Document about timings
+
+I've generically optimized my code to calculate both the minimum and maximum
+distances within the same function. This seems to have cut the time from a
+37 second processing (two seperate functions) down to approximately 17 seconds.
+Not much was changed. But the functions were condensed into one single code.
+Max distance and min distance were defined first. then the rest of the
+calculations were carried out as normal. 
