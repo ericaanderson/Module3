1b663822aa59aefd1bfce3c14afeef6200c3b287
ericaanderson <gy22ea@leeds.ac.uk>
2023-04-23 13:15:45 -0400

    abm7 updates

---
+++ src/abm7/model7.py
@@ -0,0 +1,192 @@
+
+import imageio
+import os
+import random
+import matplotlib.pyplot as plt
+import math
+import operator
+import my_modules_7.agentframework as af 
+import my_modules_7.io as io
+import my_modules_7.geometry as geometry 
+import matplotlib.animation as anim
+
+
+environment, n_rows, n_cols = io.read_data()
+
+#set the pseudo-random seed for reproducibility
+random.seed(0)
+
+# Create a variable to store the number of agents
+n_agents = 10
+
+#iterations to make the model "move" more than once. 
+n_iterations = 10
+
+neighborhood = 20
+
+# Variables for constraining movement.
+# The minimum x coordinate.
+x_min = 0
+# The minimum y coordinate.
+y_min = 0
+# The maximum an agents x coordinate is allowed to be.
+x_max = n_cols - 1
+# The maximum an agents y coordinate is allowed to be.
+y_max = n_rows - 1
+
+
+
+def get_both_distance():
+    """
+    Finds the largest maximum distance and lowest minimum between all the agents'
+           
+    Returns
+    minimum and maximum distance between all the agents
+    """
+    max_distance = 0
+    min_distance = math.inf
+    for i in range(len(agents)):
+        a = agents[i] #reassign a to first variable's position in range calculation
+        for j in range(len(agents)):
+            b = agents[j]
+            #print("i", i, "j", j)
+            distance = geometry.get_distance(a.x, a.y, b.x, b.y)
+            #print("distance between", a, b, distance)
+            max_distance = max(max_distance, distance)
+            #print("max_distance", max_distance)
+            min_distance = min(min_distance, distance)
+            #print("min_distance", min_distance)
+            return min_distance, max_distance
+            #return max_distance
+
+        # Define a function that adds up all the values in environment. #
+def sum_environment():
+    env_total = sum(environment)
+    return env_total
+print (sum_environment)
+       
+ite = 1
+images = []
+    
+def plot():
+    fig.clear()
+    plt.ylim(y_min, y_max)
+    plt.xlim(x_min, x_max)
+    plt.imshow(environment)
+    for i in range(n_agents):
+        plt.scatter(agents[i].x, agents[i].y, color='black')
+        
+    # Plot the coordinate with the largest x red
+    lx = max(agents, key=operator.attrgetter('x'))
+    plt.scatter(lx.x, lx.y, color='red')
+    # Plot the coordinate with the smallest x blue
+    sx = min(agents, key=operator.attrgetter('x'))
+    plt.scatter(sx.x, sx.y, color='blue')
+    # Plot the coordinate with the largest y yellow
+    ly = max(agents, key=operator.attrgetter('y'))
+    plt.scatter(ly.x, ly.y, color='yellow')
+    # Plot the coordinate with the smallest y green
+    sy = min(agents, key=operator.attrgetter('y'))
+    plt.scatter(sy.x, sy.y, color='green')
+    
+    # For storing images
+    global ite
+    filename = '../../data/output/images/image' + str(ite) + '.png'
+    plt.savefig(filename)
+    images.append(imageio.imread(filename))
+    plt.show()
+
+
+def update(frames):
+    # Model loop
+    #for ite in range(1, n_iterations + 1):
+    print("Iteration", frames)
+    # Move agents
+    print("Move and eat")
+    for i in range(n_agents):
+        agents[i].move(x_min, y_min, x_max, y_max)
+        agents[i].eat()
+        #print(agents[i])
+    # Share store
+    print("Share")
+    # Distribute shares
+    for i in range(n_agents):
+        agents[i].share(neighborhood)
+    # Add store_shares to store and set store_shares back to zero
+    for i in range(n_agents):
+        #print(agents[i])
+        agents[i].store = agents[i].store_shares
+        agents[i].store_shares = 0
+    print(agents)
+# =============================================================================
+#     # Print the maximum distance between all the agents
+#     print("Maximum distance between all the agents", get_max_distance())
+#     # Print the total amount of resource
+#     sum_as = sum_agent_stores()
+#     print("sum_agent_stores", sum_as)
+    sum_e = sum_environment()
+    print("sum_environment", sum_e)
+#     print("total resource", (sum_as + sum_e))
+# 
+# =============================================================================
+    # Stopping condition
+    global carry_on
+    # Random
+    if random.random() < 0.1:
+        #if sum_as / n_agents > 80:
+        carry_on = False
+        print("stopping condition")
+
+    # Plot
+    plot()
+    
+def gen_function():
+    global ite
+    global carry_on
+    while (ite <= n_iterations) & (carry_on) :
+        yield ite # Returns control and waits next call.
+        ite = ite + 1
+    global data_written
+    if data_written == False:
+        # Write data
+        print("write data")
+        io.write_data('../../data/output/out.txt', environment)
+        imageio.mimsave('../../data/output/out.gif', images, fps=3)
+        data_written = True
+        
+        
+                            # Main Simulation Loop #
+
+#initialize agents#
+agents = []
+for i in range(n_agents):
+    # Create an agent
+    agents.append(af.Agent(agents, i, environment, n_rows, n_cols))
+# Animate
+# Initialise fig and carry_on
+    fig = plt.figure(figsize=(7, 7))
+    ax = fig.add_axes([0, 0, 1, 1])
+    carry_on = True
+    data_written = False
+    animation = anim.FuncAnimation(fig, update, init_func=plot, frames=gen_function, repeat=False)
+
+    print (agents, i)
+    
+    
+    
+    
+    
+
+# =============================================================================
+# # Create directory to write images to.
+# try:
+#     os.makedirs('../../data/output/images/')
+# except FileExistsError:
+#     print("path exists")
+# =============================================================================
+
+
+    
+
+
+
